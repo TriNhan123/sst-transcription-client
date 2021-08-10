8d152d6625db572d2673e28bce83f031dbcd69e8
TriNhan123 <trinhan99999@gmail.com>
2021-08-10 00:47:13 -0700

    add save and delete on front end

--- transcription-client/src/containers/Audio.js
+++ transcription-client/src/containers/Audio.js
@@ -2,6 +2,11 @@ import React, { useRef, useState, useEffect } from "react";
 import { useParams, useHistory } from "react-router-dom";
 import { API, Storage } from "aws-amplify";
 import { onError } from "../libs/errorLib";
+import Form from "react-bootstrap/Form";
+import LoaderButton from "../components/LoaderButton";
+import config from "../config";
+import "./Audio.css";
+import { s3Upload } from "../libs/awsLib";
 
 export default function Audio() {
   const file = useRef(null);
@@ -9,6 +14,8 @@ export default function Audio() {
   const history = useHistory();
   const [audio, setAudio] = useState(null);
   const [content, setContent] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [isDeleting, setIsDeleting] = useState(false);
 
   useEffect(() => {
     function loadAudio() {
@@ -34,7 +41,128 @@ export default function Audio() {
     onLoad();
   }, [id]);
 
+  function validateForm() {
+    return content.length > 0;
+  }
+  
+  function formatFilename(str) {
+    return str.replace(/^\w+-/, "");
+  }
+  
+  function handleFileChange(event) {
+    file.current = event.target.files[0];
+  }
+  
+  function saveAudio(audio) {
+    return API.put("audio", `/audio/${id}`, {
+      body: audio
+    });
+  }
+  
+  async function handleSubmit(event) {
+    let attachment;
+  
+    event.preventDefault();
+  
+    if (file.current && file.current.size > config.MAX_ATTACHMENT_SIZE) {
+      alert(
+        `Please pick a file smaller than ${
+          config.MAX_ATTACHMENT_SIZE / 1000000
+        } MB.`
+      );
+      return;
+    }
+  
+    setIsLoading(true);
+  
+    try {
+      if (file.current) {
+        attachment = await s3Upload(file.current);
+      }
+  
+      await saveAudio({
+        content,
+        attachment: attachment || audio.attachment
+      });
+      history.push("/");
+    } catch (e) {
+      onError(e);
+      setIsLoading(false);
+    }
+  }
+  
+  function deleteAudio() {
+    return API.del("audio", `/audio/${id}`);
+  }
+  
+  async function handleDelete(event) {
+    event.preventDefault();
+  
+    const confirmed = window.confirm(
+      "Are you sure you want to delete this note?"
+    );
+  
+    if (!confirmed) {
+      return;
+    }
+  
+    setIsDeleting(true);
+  
+    try {
+      await deleteAudio();
+      history.push("/");
+    } catch (e) {
+      onError(e);
+      setIsDeleting(false);
+    }
+  }
+  
   return (
-    <div className="Audio"></div>
+    <div className="Audio">
+      {audio && (
+        <Form onSubmit={handleSubmit}>
+          <Form.Group controlId="content">
+            <Form.Control
+              as="textarea"
+              value={content}
+              onChange={(e) => setContent(e.target.value)}
+            />
+          </Form.Group>
+          <Form.Group controlId="file">
+            <Form.Label>Attachment</Form.Label>
+            {audio.attachment && (
+              <p>
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href={audio.attachmentURL}
+                >
+                  {formatFilename(audio.attachment)}
+                </a>
+              </p>
+            )}
+            <Form.Control onChange={handleFileChange} type="file" />
+          </Form.Group>
+          <LoaderButton
+            block
+            size="lg"
+            type="submit"
+            isLoading={isLoading}
+            disabled={!validateForm()}
+          >
+            Save
+          </LoaderButton>
+          <LoaderButton
+            block
+            size="lg"
+            variant="danger"
+            onClick={handleDelete}
+            isLoading={isDeleting}
+          >
+            Delete
+          </LoaderButton>
+        </Form>
+      )}
+    </div>
   );
 }
